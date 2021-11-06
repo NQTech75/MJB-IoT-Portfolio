0f6f783f9d3890aaf4ee748b4794021039ae856d
NQTech75 <74656484+NQTech75@users.noreply.github.com>
2021-11-06 08:19:57 +0800

    feature/provider:Basic API created

--- monitor_ii/my_app.py
+++ monitor_ii/my_app.py
@@ -4,6 +4,51 @@ File:           my_app.py
 Author:         Matt Barton V244576
 
 Description:
-Monitor API 
+Monitor API
 
 """
+import psutil
+from flask import Flask, render_template
+
+my_app = Flask(__name__)
+
+
+@my_app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@my_app.route('/api/device-load')
+def handle_form():
+    cpu_load = psutil.cpu_percent()
+    return {'CPU Load': cpu_load}
+
+
+@my_app.route("/api/environment")
+def get_api_environment():
+    return {"error": "Route not implemented", "temperature": None, "pressure": None, "humidity": None}
+
+
+@my_app.route("/api/temperature")
+def get_api_temperature():
+    return {"error": "Route not implemented", "temperature": None, "pressure": None, "humidity": None}
+
+
+@my_app.route("/api/pressure")
+def get_api_pressure():
+    return {"error": "Route not implemented", "temperature": None, "pressure": None, "humidity": None}
+
+
+@my_app.route("/api/humidity")
+def get_api_humidity():
+    return {"error": "Route not implemented", "temperature": None, "pressure": None, "humidity": None}
+
+
+@my_app.route("/api/does-not-exist")
+def get_api_does_not_exist():
+    return {"error": "Route not implemented", "temperature": None, "pressure": None, "humidity": None}
+
+
+if __name__ == '__main__':
+    my_app.debug = True
+    my_app.run()
