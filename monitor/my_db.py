062c7d33e77c919d33a0138fa1779b2a4bb71092
NQTech75 <74656484+NQTech75@users.noreply.github.com>
2021-11-12 16:31:10 +0800

    feature/environment: Environmental Data Class

--- monitor/my_db.py
+++ monitor/my_db.py
@@ -10,6 +10,8 @@ Sets location for database file
 
 import os
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, DateTime, Float, func, Integer, String
+from datetime import datetime
 
 db_folder = "./data/"
 db_filename = "monitoring_cpu.db"
@@ -18,3 +20,24 @@ if not os.path.isdir(db_folder):
     os.makedirs(db_folder)
     print(f"Created {db_folder} folder")
 Base = declarative_base()
+
+
+class EnvironmentTPH(Base):
+    __tablename__ = 'tph_storage'
+    id = Column(Integer, primary_key=True, autoincrement="auto")
+    device_name = Column(String)
+    device_mac = Column(String)
+    device_serial = Column(String)
+    temperature = Column(Float)
+    pressure = Column(Float)
+    humidity = Column(Float)
+    created_at = Column(DateTime)
+
+    def __init__(self):
+        self.device_name = 'UNKNOWN'
+        self.device_mac = 'ZZ:ZZ:ZZ:ZZ:ZZ:ZZ'
+        self.device_serial = 'UNKNOWN'
+        self.temperature = None
+        self.pressure = None
+        self.humidity = None
+        self.created_at = datetime.now()
